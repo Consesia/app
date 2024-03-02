027dbdf4ddff6f20316ca8afad37af2f3f443949
Azim Ahmed Warsi <98772333+AzimWarsii@users.noreply.github.com>
2024-03-02 12:46:02 +0000

    Flow config

---
+++ flow/config.js
@@ -0,0 +1,12 @@
+import { config } from "@onflow/fcl";
+
+config({
+  "app.detail.title": "Consesia",
+  "app.detail.icon": "https://i.ibb.co/1GZncpD/logo-mevota.png",
+  "flow.network": "testnet",
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "discovery.authn.endpoint":
+  "https://fcl-discovery.onflow.org/api/testnet/authn",
+  "0xConsesia": "0xbad97359ec664292",
+});
