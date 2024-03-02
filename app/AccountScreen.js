ed7393024815c21f18d65c101cf3e3362c5dd50b
Azim Ahmed Warsi <98772333+AzimWarsii@users.noreply.github.com>
2024-03-02 22:22:17 +0530

    Update AccountScreen.js

--- app/AccountScreen.js
+++ app/AccountScreen.js
@@ -4,6 +4,7 @@ import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { styled } from "nativewind";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useEffect, useState } from "react";
+import { RefreshControl } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import { getAllPolls } from "../flow/scripts";
 import { filter, reverse } from "lodash";
@@ -16,6 +17,7 @@ export default function AccountScreen({ navigation }) {
   const [tab, setTab] = useState("poll");
   const [user, setUser] = useState({ loggedIn: null });
   const [myPolls, setMyPolls] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
   const [myVoted, setMyVoted] = useState([]);
 
   useEffect(() => fcl.currentUser.subscribe(setUser), []);
@@ -27,10 +29,20 @@ export default function AccountScreen({ navigation }) {
   const fetchMyPolls = async () => {
     getAllPolls()
       .then((res) => {
-        setMyPolls(reverse(Object.values(res).filter((i) => i.createdBy == user.addr)));
-        setMyVoted(filter(Object.values(res), (o) => o.votes.hasOwnProperty(user.addr)))
+        setMyPolls(
+          reverse(Object.values(res).filter((i) => i.createdBy == user.addr))
+        );
+        setMyVoted(
+          filter(Object.values(res), (o) => o.votes.hasOwnProperty(user.addr))
+        );
       })
       .catch((err) => console.log(err));
+  };
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await fetchMyPolls();
+    setRefreshing(false);
   };
 
   return (
@@ -45,26 +57,68 @@ export default function AccountScreen({ navigation }) {
     >
       <StatusBar style="light" />
       <StyledView className="h-48 flex items-center justify-center">
-        <TouchableOpacity className="absolute top-3 right-3" onPress={() => fcl.unauthenticate()}>
+        <TouchableOpacity
+          className="absolute top-3 right-3"
+          onPress={() => fcl.unauthenticate()}
+        >
           <Feather name="log-out" size={24} color="white" />
         </TouchableOpacity>
         <StyledView className="border-2 w-24 h-24 p-1.5 border-white/20 rounded-full border-dashed mb-3">
-          <Image className="w-20 h-20 object-cover rounded-full" source={{ uri: `https://www.gravatar.com/avatar/${user?.addr}?s=200&r=pg&d=retro` }} />
+          <Image
+            className="w-20 h-20 object-cover rounded-full"
+            source={{
+              uri: `https://www.gravatar.com/avatar/${user?.addr}?s=200&r=pg&d=retro`,
+            }}
+          />
         </StyledView>
         <Text className="text-white text-lg">{user?.addr}</Text>
       </StyledView>
       <StyledView className="bg-dark flex-1 px-3 pt-3">
         <StyledView className="flex flex-row items-center gap-4 mb-3">
-          <TouchableOpacity className="flex-1 h-10 rounded-full" onPress={() => setTab("poll")}>
-            <StyledView className={`h-full rounded-full flex items-center justify-center flex-row ${tab === "poll" && "bg-white"}`}>
-              <Feather name="layers" size={16} color={`${tab === "poll" ? "#222" : "#fff"}`} />
-              <Text className={`font-medium ml-2 ${tab === "poll" ? "text-dark" : "text-white"}`}>My Poll</Text>
+          <TouchableOpacity
+            className="flex-1 h-10 rounded-full"
+            onPress={() => setTab("poll")}
+          >
+            <StyledView
+              className={`h-full rounded-full flex items-center justify-center flex-row ${
+                tab === "poll" && "bg-white"
+              }`}
+            >
+              <Feather
+                name="layers"
+                size={16}
+                color={`${tab === "poll" ? "#222" : "#fff"}`}
+              />
+              <Text
+                className={`font-medium ml-2 ${
+                  tab === "poll" ? "text-dark" : "text-white"
+                }`}
+              >
+                My Poll
+              </Text>
             </StyledView>
           </TouchableOpacity>
-          <TouchableOpacity className="flex-1 h-10 rounded-full" onPress={() => setTab("voted")}>
-            <StyledView className={`h-full rounded-full flex items-center justify-center flex-row ${tab === "voted" && "bg-white"}`}>
-              <Feather name="check-circle" size={16} color={`${tab === "voted" ? "#222" : "#fff"}`} />
-              <Text className={`font-medium ml-2 ${tab === "voted" ? "text-dark" : "text-white"}`}>My Voted</Text>
+          <TouchableOpacity
+            className="flex-1 h-10 rounded-full"
+            onPress={() => setTab("voted")}
+          >
+            <StyledView
+              className={`h-full rounded-full flex items-center justify-center flex-row ${
+                tab === "voted" && "bg-white"
+              }`}
+            >
+              <Feather
+                name="check-circle"
+                size={16}
+                color={`${tab === "voted" ? "#222" : "#fff"}`}
+              />
+              <Text
+                className={`font-medium ml-2 ${
+                  tab === "voted" ? "text-dark" : "text-white"
+                }`}
+              >
+                My Voted
+              </Text>
             </StyledView>
           </TouchableOpacity>
         </StyledView>
@@ -72,18 +126,46 @@ export default function AccountScreen({ navigation }) {
           <FlatList
             data={myPolls}
             renderItem={({ item }) => (
-              <VoteItem pollId={item.id} title={item.title} color={item.color} createdBy={item.createdBy} time={item.endedAt} navigation={navigation} cta="Detail" />
+              <VoteItem
+                pollId={item.id}
+                title={item.title}
+                color={item.color}
+                createdBy={item.createdBy}
+                time={item.endedAt}
+                navigation={navigation}
+                cta="Detail"
+              />
             )}
             keyExtractor={(item) => item.id}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={handleRefresh}
+              />
+            }
           />
         )}
         {tab === "voted" && (
           <FlatList
             data={myVoted}
             renderItem={({ item }) => (
-              <VoteItem pollId={item.id} title={item.title} color={item.color} createdBy={item.createdBy} time={item.endedAt} navigation={navigation} cta="Detail" />
+              <VoteItem
+                pollId={item.id}
+                title={item.title}
+                color={item.color}
+                createdBy={item.createdBy}
+                time={item.endedAt}
+                navigation={navigation}
+                cta="Detail"
+              />
             )}
             keyExtractor={(item) => item.id}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={handleRefresh}
+              />
+            }
           />
         )}
       </StyledView>
